b700774c28c5d24e26494efdb98e7e98f37a964f
jpardogo <jpardogo@gmail.com>
2013-11-12 22:14:58 +0000

    remove unused settings option menu

--- ListBuddies/src/main/java/com/jpardogo/android/listbuddies/ui/MainActivity.java
+++ ListBuddies/src/main/java/com/jpardogo/android/listbuddies/ui/MainActivity.java
@@ -1,9 +1,7 @@
 package com.jpardogo.android.listbuddies.ui;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 import com.jpardogo.android.listbuddies.R;
 import com.jpardogo.android.listbuddies.ui.fragments.ListBuddiesFragment;
@@ -22,27 +20,5 @@ public class MainActivity extends ActionBarActivity {
                     .commit();
         }
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 
 }
