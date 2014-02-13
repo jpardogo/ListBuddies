fe6199938116d60d52142db94bf25bc724f635c8
jpardogo <jpardogo@gmail.com>
2014-02-13 00:26:30 +0000

    Update example

--- example/src/main/java/com/jpardogo/android/listbuddies/ui/ExampleActivity.java
+++ example/src/main/java/com/jpardogo/android/listbuddies/ui/ExampleActivity.java
@@ -2,14 +2,21 @@ package com.jpardogo.android.listbuddies.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageView;
 
 import com.jpardogo.android.listbuddies.R;
+import com.squareup.picasso.Picasso;
 
 public class ExampleActivity extends ActionBarActivity {
+
+    public static final String EXTRA_URL = "url";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.example_activity);
+        String imageUrl = getIntent().getExtras().getString(EXTRA_URL);
+        Picasso.with(this).load(imageUrl).fit().centerCrop().into((ImageView) findViewById(R.id.image));
+
     }
 }
