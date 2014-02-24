fc6402aacc4073d352bedec1d9c2378c58611d70
jpardogo <jpardogo@gmail.com>
2014-02-24 01:06:36 +0000

    Scroll config options inverse

--- library/src/main/java/com/jpardogo/listbuddies/lib/provider/ScrollConfigOptions.java
+++ library/src/main/java/com/jpardogo/listbuddies/lib/provider/ScrollConfigOptions.java
@@ -4,8 +4,9 @@ package com.jpardogo.listbuddies.lib.provider;
  * Created by jpardogo on 23/02/2014.
  */
 public enum ScrollConfigOptions {
-    LEFT,
-    RIGHT;
+    RIGHT,
+    LEFT;
+
 
     public int getConfigValue() {
         return ordinal() + 1;
