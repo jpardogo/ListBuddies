f5d6bf85c590c55569df5baf1b9df6278a470944
jpardogo <jpardogo@gmail.com>
2014-02-24 01:06:43 +0000

    Parent activity navigation and remove transition animation

--- example/src/main/java/com/jpardogo/android/listbuddies/ui/DetailActivity.java
+++ example/src/main/java/com/jpardogo/android/listbuddies/ui/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.jpardogo.android.listbuddies.ui;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.jpardogo.android.listbuddies.R;
@@ -13,10 +15,30 @@ public class DetailActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        overridePendingTransition(0, 0);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.example_activity);
+        setContentView(R.layout.detail_activity);
         String imageUrl = getIntent().getExtras().getString(EXTRA_URL);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
         Picasso.with(this).load(imageUrl).fit().centerCrop().into((ImageView) findViewById(R.id.image));
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        overridePendingTransition(0, 0);
+    }
 }
+
+
